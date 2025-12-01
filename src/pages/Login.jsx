f9b5dd0fd1acbbf791a0600e8cc83f399e96aba7
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-12-01 21:39:23 +0000

    VCT-80 small fix in the login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,33 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom"
-import { useUser } from "../context/UserContext";
+import NavBar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 function Login() {
-    const navigate = useNavigate();
-    const { setUser } = useUser();
+  // const navigate = useNavigate();
+  // const { setUser } = useUser();
 
-    useEffect(() => {
-        // Set a hardcoded user for testing
-        const hardcodedUser = {
-            id: "123e4567-e89b-12d3-a456-426614174000",
-            birthdate: "1990-01-01",
-            rating: 4.5,
-            account: {
-                email: "test@example.com",
-                name: "Test User"
-            }
-        };
-        
-        setUser(hardcodedUser);
-        navigate("/explore");
-    }, [navigate, setUser]);
+  // useEffect(() => {
+  //     // Set a hardcoded user for testing
+  //     const hardcodedUser = {
+  //         id: "123e4567-e89b-12d3-a456-426614174000",
+  //         birthdate: "1990-01-01",
+  //         rating: 4.5,
+  //         account: {
+  //             email: "test@example.com",
+  //             name: "Test User"
+  //         }
+  //     };
 
-    return null;
+  //     setUser(hardcodedUser);
+  //     navigate("/explore");
+  // }, [navigate, setUser]);
+
+  return (
+    <>
+      <NavBar />
+      <div></div>
+      <Footer />
+    </>
+  )
 }
 
 export default Login
