a06c79dec1290ce5775235e8252274df7259ab4e
Gonçalo Simões <151958998+goncaloosimoes@users.noreply.github.com>
2025-12-16 12:05:20 +0000

    VCT-38 remove duplicate

--- src/services/listing.js
+++ src/services/listing.js
@@ -83,10 +83,3 @@ export const getListingById = async (listingId) => {
   })
   return response.data
 }
-
-export const updateListing = async (listingId, data) => {
-  const response = await axios.put(`${OWNERS_API}/listings/${listingId}`, data, {
-    headers: getAuthHeaders(),
-  })
-  return response.data
-}
